c786ae696d90bec60d0eb0b8bed5da7201991e44
Mercedes Delin <mercedesgdelin@gmail.com>
2019-10-16 15:31:21 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
+def hello_t (array)
+  #i is the counter for this loop
+  #if else statement allows code to be flexible, and not break when no block is given ./
+  if block_given?
+    i=0
+  while i < array.length
+  yield array[i]
+  i=i+1
+end
+return array
+else 
+  puts "Hey! No block was given!"
+end
 end
 
-# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
